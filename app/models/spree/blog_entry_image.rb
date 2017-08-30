4a160afaa51fe4f97c174fab49202ed3c33e47dc
Dan Mitchell <dan@digitalreflow.co.uk>
2017-08-30 23:38:22 +0100

    Fix blog_entry_image path

--- app/models/spree/blog_entry_image.rb
+++ app/models/spree/blog_entry_image.rb
@@ -1,5 +1,6 @@
 class Spree::BlogEntryImage < Spree::Image
   has_attached_file :attachment,
   :styles => {:mini => '48x48#', :normal => '150x150#', :large => '1024x600>'},
+  :path => ":rails_root/public/assets/blog_entry_images/:id/:style/:basename.:extension",
   :url => "/assets/blog_entry_images/:id/:style/:basename.:extension"
 end
